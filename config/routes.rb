a739315bb2b5e76706f9c3dae61a6f116ba1cc84
aelphy <miha-usv@yandex.ru>
2013-01-24 12:33:31 +0600

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Badgenator::Application.routes.draw do
   match 'contacts' => 'welcome#contacts', :as => :contacts
-  match 'archive' => 'welcome#archive'
-  match 'archive/new' => 'badge_sets#create'
-  
+
+  #ceты
+  resources :archive, :controller => :badge_sets
+  match 'archive/page/:page' => 'badge_sets#index', :via => :get
+
+  #бэйджи
+  resources :badge_set, :controller => :badges, :path => "archive" do 
+    resources :badges do
+      collection do
+        get 'page/:page', :action => :index
+      end
+    end
+  end 
+   
   root :to => 'welcome#index'
 end
