da93d22a3c6c78764f9fac9d3b90d2f621ea32c7
Mikhail Usvyatsov <miha-usv@yandex.ru>
2013-03-08 15:55:00 +0600

    Update 20130308094218_good.rb

--- db/migrate/20130308094218_good.rb
+++ db/migrate/20130308094218_good.rb
@@ -1,15 +1,7 @@
-<<<<<<< HEAD:db/migrate/20130308094218_good.rb
 class Good < ActiveRecord::Migration
   def up
     change_table :badge_sets do |t|
       t.string :source      
     end
-=======
-class Addrow < ActiveRecord::Migration
-  def up
-    change_table :badge_sets do |t|
-      t.string :source      
-    end    
->>>>>>> 1d8beada6ba3718e7a38f5d9230dd942ae13f83b:db/migrate/20130306053505_csv.rb
   end
 end
