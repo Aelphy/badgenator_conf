fa9023e92f49021c084c82c59a4a6e59fad2814f
aelphy <miha-usv@yandex.ru>
2013-03-11 12:30:02 +0600

    iport improves, fast destroy

--- app/models/badge_set.rb
+++ app/models/badge_set.rb
@@ -12,7 +12,7 @@ class BadgeSet < ActiveRecord::Base
   mount_uploader :image, ImageUploader  
   mount_uploader :source, CsvUploader
   
-  validates :name, presence: true, length: { :minimum => 2, :maximum => 50 }  
+  validates :name, presence: true, length: {:minimum => 2, :maximum => 50}  
   
   before_validation :set_name
   
@@ -27,9 +27,7 @@ class BadgeSet < ActiveRecord::Base
   
   def check_source
     unless source.blank?
-      self.badges.each do |badge|
-        badge.delete
-      end
+      self.badges.delete_all
 
       import(source.url)
     end
@@ -40,12 +38,10 @@ class BadgeSet < ActiveRecord::Base
   def import(file)
     csv_text = File.read(File.join("public", file))
 
-    line = CSV.parse(csv_text)
+    line = CSV.parse(csv_text, col_sep: "\t")
 
     line.each do |row|
-      values = row[0].split("\t")
-
-      badge = self.badges.new(name: values[0], surname: values[1], company: values[2], profession: values[3])
+      badge = self.badges.new(name: row[0], surname: row[1], company: row[2], profession: row[3])
 
       if badge.valid?    
         badge.save!
